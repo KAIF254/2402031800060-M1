f81392aab694dc78d70b93092ba9c79478d7fa1f
kaif shaikh <156075872+KAIF254@users.noreply.github.com>
2026-05-21 10:28:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import reactlogo from './assets/react.svg'
+//import { useState } from 'react'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+//import './App.css'
+import Hello from  "./Hello"
+
+function App() {
+  
+  return (
+    <>
+        <h1>kaif shaikh</h1>
+    <Hello />
+<img src={reactlogo} width="50px" />    </>
+  )
+}
+
+export default App
